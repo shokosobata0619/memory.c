1efab3580c45f3ba2354267c45a5a83250ef7f1b
shokosobata0619 <sh8444@nyu.edu>
2025-11-22 21:45:12 -0500

    Update memory.c

--- memory.c
+++ memory.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id){
+    // changed allo_mem -> allocated_memory
     struct MEMORY_BLOCK temp_memory_block, allocated_memory;
     allocated_memory.end_address = 0;
     allocated_memory.start_address = 0;
@@ -14,44 +15,57 @@ struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memo
 
     //find the candidate memory blocks that can be allocated and chooses the one whose size is closest to the requested size
     for(int i = 0; i <= *map_cnt - 1; i++){
-      if(request_size == memory_map[i].segment_size){
-        match = true;
-        memory_map_index = i;
-        best_fit_segment = request_size;
-        break;
-      }
-      else if((memory_map[i].segment_size > request_size)&&(memory_map[i].process_id == 0)) {
-        if(match==false){
-          match=true;
-          memory_map_index = i;
-          best_fit_segment = memory_map[i].segment_size;
-        }
-        else{
-          if(best_fit_segment > memory_map[i].segment_size){
+        if(request_size == memory_map[i].segment_size && memory_map[i].process_id == 0){
+            match = true;
             memory_map_index = i;
-            best_fit_segment = memory_map[i].segment_size;
-          }
+            best_fit_segment = request_size;
+            break;
         }
-      }
+        else if((memory_map[i].segment_size > request_size) && (memory_map[i].process_id == 0)) {
+            if(match == false){
+                match = true;
+                memory_map_index = i;
+                best_fit_segment = memory_map[i].segment_size;
+            }
+            else{
+                if(best_fit_segment > memory_map[i].segment_size){
+                    memory_map_index = i;
+                    best_fit_segment = memory_map[i].segment_size;
+                }
+            }
+        }
     }
 
+    if (match == true){
+        temp_memory_block = memory_map[memory_map_index];
 
-if (match==true){
-    temp_memory_block = memory_map[memory_map_index];
-    allocated_memory.start_address = temp_memory_block.start_address;
-    allocated_memory.end_address = allocated_memory.start_address + request_size - 1;
-    allocated_memory.process_id = process_id;
-    allocated_memory.segment_size = request_size;
-    *map_cnt = *map_cnt + 1;
+        if (temp_memory_block.segment_size == request_size) {
+            memory_map[memory_map_index].process_id = process_id;
+            allocated_memory = memory_map[memory_map_index];
+            return allocated_memory;
+        }
 
-    for(int i = memory_map_index; i <= *map_cnt; i++){
-        memory_map[memory_map_index+1] = memory_map[i];
+        // Otherwise, split the block
+        allocated_memory.start_address = temp_memory_block.start_address;
+        allocated_memory.end_address = allocated_memory.start_address + request_size - 1;
+        allocated_memory.process_id = process_id;
+        allocated_memory.segment_size = request_size;
+
+        // Increase map count first
+        *map_cnt = *map_cnt + 1;
+
+        for(int i = *map_cnt - 1; i > memory_map_index; i--){
+            memory_map[i] = memory_map[i - 1];
+        }
+
+        // Now create the remaining free block at memory_map_index + 1
+        memory_map[memory_map_index] = allocated_memory;
+        memory_map[memory_map_index + 1].start_address = allocated_memory.end_address + 1;
+        memory_map[memory_map_index + 1].end_address = temp_memory_block.end_address;
+        memory_map[memory_map_index + 1].process_id = 0;
+        memory_map[memory_map_index + 1].segment_size = temp_memory_block.segment_size - request_size;
     }
-    memory_map[memory_map_index+1].start_address = allocated_memory.end_address +1;
-    memory_map[memory_map_index+1].end_address = memory_map[memory_map_index].end_address;
-    memory_map[memory_map_index+1].process_id = memory_map[memory_map_index].process_id;
-    memory_map[memory_map_index+1].segment_size = memory_map[memory_map_index].segment_size - request_size;
-    memory_map[memory_map_index] = allocated_memory;
-}
-return allocated_memory;
+
+    // If match == false, allocated_memory is still {0,0,0,0} (NULLBLOCK)
+    return allocated_memory;
 }
