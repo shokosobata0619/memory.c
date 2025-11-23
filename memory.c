6a949dd24fe7e3bf84c543b865f93adbe7c7bc3e
shokosobata0619 <sh8444@nyu.edu>
2025-11-22 21:43:06 -0500

    Update memory.c

--- memory.c
+++ memory.c
@@ -4,11 +4,11 @@
 #include <stdbool.h>
 
 struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id){
-    struct MEMORY_BLOCK temp_memory_block, allo_mem;
-    allo_mem.end_address = 0;
-    allo_mem.start_address = 0;
-    allo_mem.process_id = 0;
-    allo_mem.segment_size = 0; 
+    struct MEMORY_BLOCK temp_memory_block, allocated_memory;
+    allocated_memory.end_address = 0;
+    allocated_memory.start_address = 0;
+    allocated_memory.process_id = 0;
+    allocated_memory.segment_size = 0; 
     bool match = false;
     int memory_map_index = 0, best_fit_segment = 0;
 
@@ -38,20 +38,20 @@ struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memo
 
 if (match==true){
     temp_memory_block = memory_map[memory_map_index];
-    allo_memory.start_address = temp_memory_block.start_address;
-    allo_memory.end_address = allo_memory.start_address + request_size - 1;
-    allo_memory.process_id = process_id;
-    allo_memory.segment_size = request_size;
+    allocated_memoryory.start_address = temp_memory_block.start_address;
+    allocated_memoryory.end_address = allocated_memoryory.start_address + request_size - 1;
+    allocated_memoryory.process_id = process_id;
+    allocated_memoryory.segment_size = request_size;
     *map_cnt = *map_cnt + 1;
 
     for(int i = memory_map_index; i <= *map_cnt; i++){
         memory_map[memory_map_index+1] = memory_map[i];
     }
-    memory_map[memory_map_index+1].start_address = allo_memory.end_address +1;
+    memory_map[memory_map_index+1].start_address = allocated_memoryory.end_address +1;
     memory_map[memory_map_index+1].end_address = memory_map[memory_map_index].end_address;
     memory_map[memory_map_index+1].process_id = memory_map[memory_map_index].process_id;
     memory_map[memory_map_index+1].segment_size = memory_map[memory_map_index].segment_size - request_size;
-    memory_map[memory_map_index] = allo_memory;
+    memory_map[memory_map_index] = allocated_memoryory;
 }
-return allo_memory;
+return allocated_memoryory;
 }
